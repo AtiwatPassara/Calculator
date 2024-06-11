2f15ba8120d94f90576bdd060511270cf1aa6ffb
AtiwatPassara <act22040@gmail.com>
2024-06-11 16:27:42 -0400

    Home page create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { Input } from "@nextui-org/input";
+
 export default function Page() {
-  return <h1 className="text-3xl font-bold underline text-red-500">Hello, Next.js!</h1>
+  return (
+    <div className="flex items-center justify-around">
+      <div className="text-5xl text-white max-w-md">
+        <div className="font-bold">
+          This website is for calculating
+            <div >
+              <span className="text-green-500">Environmental Impact</span> of
+              <span className="text-yellow-500"> Biking</span>
+            </div>
+        </div>
+        <br />
+        <div className="text-[15px] mt-2 leading-relaxed">
+          This website is designed to help you understand the environmental impact of biking. 
+          By entering simple details about your biking activities, such as the duration of your 
+          rides and your food consumption, you can calculate the positive effects your biking has on the environment.
+        </div>
+      </div>
+
+      <div>
+        <h5 className="text-5xl">Picture</h5>
+      </div>
+    </div>
+  );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,8 @@ import Links from "./links/Links";
 
 const Navbar : React.FC = () => {
     return (
-        <div className="flex items-center justify-between mt-4 px-4" >
-                <div className="font-bold text-[24px]">
+        <div className="flex items-center justify-between mt-4 px-4 py-4 border-b-2" >
+                <div className="font-bold text-[24px] ">
                     LOGO
                 </div>
                 <div >
