4c335f6b6b04a69b034f0aa5241bd6841eff396e
AtiwatPassara <act22040@gmail.com>
2024-06-12 14:02:22 -0400

    Responsive navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
 import { Input } from "@nextui-org/input";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-around">
-      <div className="text-5xl text-white max-w-md">
-        <div className="font-bold">
+    <div className="flex items-center justify-around p-8">
+      <div className="text-white max-w-lg">
+        <div className="text-5xl font-bold leading-tight">
           This website is for calculating
-            <div >
-              <span className="text-green-500">Environmental Impact</span> of
-              <span className="text-yellow-500"> Biking</span>
-            </div>
+          <div>
+            <span className="text-green-500">Environmental Impact</span> of
+            <span className="text-yellow-500"> Biking</span>
+          </div>
         </div>
-        <br />
-        <div className="text-[15px] mt-2 leading-relaxed">
-          This website is designed to help you understand the environmental impact of biking. 
-          By entering simple details about your biking activities, such as the duration of your 
-          rides and your food consumption, you can calculate the positive effects your biking has on the environment.
+        <div className="flex items-center justify-between gap-8 mt-8">
+          <div className="text-[15px] leading-relaxed">
+            This website is designed to help you understand the environmental impact of biking. 
+            You can calculate the positive effects your biking has on the environment.
+          </div>
+          <Link href={"/calculator"}>
+            <button className="flex items-center border-4 border-white rounded-lg p-5 text-[28px] hover:bg-black hover:border-yellow-500 hover:text-yellow-500 transition-all hover:scale-110 duration-300 ease-in-out">
+            Calculate
+            </button>
+          </Link>
         </div>
       </div>
-
-      <div>
-        <h5 className="text-5xl">Picture</h5>
+      <div className="hidden text-5xl text-white lg:block">
+        Picture
       </div>
     </div>
   );
--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -1,29 +1,50 @@
-import Link from "next/link"
+"use client"
 import NavLink from "./navLink/navLink"
+import { useState } from "react"
 
-const Links : React.FC = () => {
-
-    const links = [{
-        title: "Home",
-        path:"/",
+const Links: React.FC = () => {
+  const links = [
+    {
+      title: "Home",
+      path: "/",
     },
     {
-        title: "About",
-        path:"/about",
+      title: "About",
+      path: "/about",
     },
     {
-        title: "Contact",
-        path:"/contact",
+      title: "Contact",
+      path: "/contact",
     },
     {
-        title: "Calculator",
-        path:"/calculator",
-    }]
+      title: "Calculator",
+      path: "/calculator",
+    },
+  ]
+  
+  const [open, setOpen] = useState(false)
 
-    return  <div className="flex align-item-center justify-between gap-5 ">
-                {links.map((link => (
-                    <NavLink item={link} key={link.title} />
-                )))}
-            </div>
+  return (
+    <div>
+      <div className="hidden md:flex items-center justify-between gap-5">
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+      </div>
+      <button className="flex md:hidden" onClick={() => setOpen((prev) => !prev)}>
+        Menu
+      </button>
+      {open && (
+        <div className="flex flex-col items-center justify-center fixed top-0 right-0 w-[50%] min-h-screen gap-[10px] bg-black md:hidden">
+          <button className="flex md:hidden" onClick={() => setOpen((prev) => !prev)}>
+        Menu
+      </button>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 export default Links;
