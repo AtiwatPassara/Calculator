e662028bea62f10765ba287e3bfdbd5c3b7462f7
AtiwatPassara <act22040@gmail.com>
2024-07-11 15:01:12 -0400

    Input material filed for classic bile

--- src/components/modal/bikeModal.tsx
+++ src/components/modal/bikeModal.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { IoIosClose } from "react-icons/io";
 import ClassicModal from '../modal/classicModal'; // Correct import path
@@ -7,6 +8,7 @@ const selectMaterial = [
   { title: "-", value: "-" },
   { title: "Bamboo", value: "ฺbamboo" },
   { title: "Carbon", value: "carbon" },
+  { title: "Aluminium", value: "aluminium" }
 ];
 
 const selectType = [
@@ -48,6 +50,22 @@ const BikeModal: React.FC<BikeModalProps> = ({
 }) => {
   if (!showModal) return null;
 
+  const [isDisabled,setIsDisabled] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleDisable = () => {
+      if(selectedBike === "Classic"){
+        setIsDisabled(false)
+      }
+      else{
+        setIsDisabled(true)
+      }
+    }
+  handleDisable()
+  },[selectedBike])
+
+  
+
   return (
     <div className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
       <div className="bg-black m-auto p-8 w-[900px] h-auto rounded-md shadow-lg border border-gray-500">
@@ -59,7 +77,7 @@ const BikeModal: React.FC<BikeModalProps> = ({
         <div className="flex flex-col items-center mt-5">
           <p className="text-center text-xl border-b-2 pb-2 text-white border-red-500">Select your bike</p>
           <div className="h-full flex justify-center items-center mt-5 p-5 gap-5 rounded-md">
-            <button onClick={() => { handleBikeSelection("Classic"); }}>
+            <button onClick={() => { handleBikeSelection("Classic");}}>
               <div className={`transition-all duration-300 relative w-80 h-64 border p-5 rounded-md ${selectedBike === "Classic" ? "bg-white" : "bg-black"} hover:border-yellow-500 flex items-center justify-center`}>
                 <div style={{ maxWidth: '200px', maxHeight: '200px', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                   <Image src="/asset/classic.png" alt="Classic" width={200} height={200} objectFit="contain" />
@@ -69,7 +87,7 @@ const BikeModal: React.FC<BikeModalProps> = ({
                 </div>
               </div>
             </button>
-            <button onClick={() => handleBikeSelection("Cargo")}>
+            <button onClick={() => {handleBikeSelection("Cargo")}}>
               <div className={`transition-all duration-300 relative w-80 h-64 border p-5 rounded-md ${selectedBike === "Cargo" ? "bg-white" : "bg-black"} hover:border-yellow-500 flex items-center justify-center`}>
                 <div style={{ maxWidth: '260px', maxHeight: '200px', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                   <Image src="/asset/cargobike.png" alt="Cargo" width={260} height={200} objectFit="contain" />
@@ -84,8 +102,8 @@ const BikeModal: React.FC<BikeModalProps> = ({
               <select
                 className="bg-black text-white p-2 border border-white rounded-md focus:border-green-500 m-2"
                 onChange={(e) => handleMaterialSelection(e.target.value)}
-                value={selectedMaterial || "-"}
-                disabled={false}
+                
+                disabled={isDisabled}
               >
                 {selectMaterial.map((option) => (
                   <option value={option.value} key={option.value}>
@@ -99,7 +117,7 @@ const BikeModal: React.FC<BikeModalProps> = ({
               <select
                 className="bg-black text-white p-2 border border-white rounded-md focus:border-green-500 m-2"
                 onChange={(e) => handlePowerSelection(e.target.value)}
-                value={selectedPower || "-"}
+                
               >
                 {selectType.map((option) => (
                   <option value={option.value} key={option.value}>
