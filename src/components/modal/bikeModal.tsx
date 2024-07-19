695a01e4da6a8a69778176ba021a87e9aa7ef512
AtiwatPassara <act22040@gmail.com>
2024-07-19 17:27:55 -0400

    Update bikeModal.tsx

--- src/components/modal/bikeModal.tsx
+++ src/components/modal/bikeModal.tsx
@@ -55,7 +55,7 @@ const BikeModal: React.FC<BikeModalProps> = ({
 
   return (
     <div className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
-      <div className="bg-black m-auto p-8 w-[900px] h-auto rounded-md shadow-lg border border-gray-500">
+      <div className="bg-black m-auto p-8 md:w-[900px] w-screen h-auto rounded-md shadow-lg border border-gray-500">
         <div className="flex justify-end">
           <button type="button" className="text-white p-2 rounded" onClick={handleClose} aria-label="Close Modal">
             <IoIosClose size={25} />
@@ -63,9 +63,9 @@ const BikeModal: React.FC<BikeModalProps> = ({
         </div>
         <div className="flex flex-col items-center mt-5">
           <p className="text-center text-xl border-b-2 pb-2 text-white border-red-500">Select your bike</p>
-          <div className="h-full flex justify-center items-center mt-5 p-5 gap-5 rounded-md">
+          <div className="h-full flex flex-col md:flex-row justify-center items-center mt-5 p-5 gap-5 rounded-md">
             <button onClick={() => { handleBikeSelection("Classic"); }}>
-              <div className={`transition-all duration-300 relative w-80 h-64 border p-5 rounded-md ${selectedBike === "Classic" ? "bg-white" : "bg-black"} hover:border-yellow-500 flex items-center justify-center`}>
+              <div className={`transition-all duration-300 relative w-60 h-40 md:w-80 md:h-64 border p-5 rounded-md ${selectedBike === "Classic" ? "bg-white" : "bg-black"} hover:border-yellow-500 flex items-center justify-center`}>
                 <div style={{ maxWidth: '200px', maxHeight: '200px', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                   <Image src="/asset/classic.png" alt="Classic" width={200} height={200} objectFit="contain" />
                 </div>
@@ -75,7 +75,7 @@ const BikeModal: React.FC<BikeModalProps> = ({
               </div>
             </button>
             <button onClick={() => { handleBikeSelection("Cargo"); }}>
-              <div className={`transition-all duration-300 relative w-80 h-64 border p-5 rounded-md ${selectedBike === "Cargo" ? "bg-white" : "bg-black"} hover:border-yellow-500 flex items-center justify-center`}>
+              <div className={`transition-all duration-300 relative w-60 h-40 md:w-80 md:h-64 border p-5 rounded-md ${selectedBike === "Cargo" ? "bg-white" : "bg-black"} hover:border-yellow-500 flex items-center justify-center`}>
                 <div style={{ maxWidth: '260px', maxHeight: '200px', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                   <Image src="/asset/cargobike.png" alt="Cargo" width={260} height={200} objectFit="contain" />
                 </div>
@@ -83,8 +83,8 @@ const BikeModal: React.FC<BikeModalProps> = ({
               </div>
             </button>
           </div>
-          <div className="flex flex-row justify-center gap-3 mt-5">
-            <div className='flex items-center p-2 rounded-md'>
+          <div className="flex flex-col md:flex-row justify-center gap-3 md:mt-5">
+            <div className='flex items-center md:p-2 rounded-md'>
               {/* Conditional class application for text color */}
               <span className={`${isDisabled ? 'text-[#aaaaaa]' : 'text-white'}`}>Select Material</span>
               <select
